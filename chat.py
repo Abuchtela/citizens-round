555b598f5a271fe38c077a56e2fe55218885c0d1
rohitmalekar <rohitmalekar@gmail.com>
2023-06-14 00:38:18 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -8,11 +8,12 @@ from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 # load from disk
 #index = GPTVectorStoreIndex.load_from_disk('index.json')
 index = load_index_from_disk(StorageContext.from_defaults(persist_dir="./storage"))
+query_engine = index.as_query_engine()
 
 # while True:
 question = st.text_input("Question", placeholder="Enter your question here")
 
 if question != "":
-    response = index.query(question)
+    response = query_engine.query(question)
     display = "\n" + "\n**" + question + "**\n" + str(response)
     st.markdown(display)
