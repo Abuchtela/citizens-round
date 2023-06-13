a38bb1c31393480c8892f0f04170150b533c4639
rohitmalekar <rohitmalekar@gmail.com>
2023-06-14 00:24:41 +0530

    Update build_index.py

--- build_index.py
+++ build_index.py
@@ -3,15 +3,17 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 from IPython.display import Markdown, display
+from llama_index import StorageContext, load_index_from_disk
 
 documents = SimpleDirectoryReader('data').load_data()
-index = GPTVectorStoreIndex(documents)
+index = GPTVectorStoreIndex.from_documents(documents)
 
 # save to disk
-index.save_to_disk('index.json')
+#index.save_to_disk('index.json')
+index.storage_context.persist(persist_dir="./storage")
 
 # load from disk
-index = GPTVectorStoreIndex.load_from_disk('index.json')
+loaded_index = load_index_from_disk(StorageContext.from_defaults(persist_dir="./storage"))
 
-response = index.query("What is Citizens Round?")
+response = loaded_index.query("What is Citizens Round?")
 print(response)
