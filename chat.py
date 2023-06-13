63e8df7f02cfe7e49b72de3b53cf95b4be5be46a
rohitmalekar <rohitmalekar@gmail.com>
2023-06-14 00:28:33 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,15 +7,13 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 
 # load from disk
-index = GPTVectorStoreIndex.load_from_disk('index.json')
+#index = GPTVectorStoreIndex.load_from_disk('index.json')
+index = load_index_from_disk(StorageContext.from_defaults(persist_dir="./storage"))
 
 # while True:
 question = st.text_input("Question", placeholder="Enter your question here")
-#st.write("You entered:", question)
-#    if question == 'quit':
-#        break
+
 if question != "":
     response = index.query(question)
     display = "\n" + "\n**" + question + "**\n" + str(response)
     st.markdown(display)
-    
