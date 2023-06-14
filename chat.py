393ee6a44516bb2f4b8263b2255f84dceb5d9d14
rohitmalekar <rohitmalekar@gmail.com>
2023-06-15 01:23:29 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -10,6 +10,15 @@ from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 index = load_index_from_storage(StorageContext.from_defaults(persist_dir="./storage"))
 query_engine = index.as_query_engine()
 
+st.title("Gitcoin Citizens Round")
+st.markdown("[The Gitcoin Citizens Round](https://gov.gitcoin.co/t/rewarding-the-community-gitcoin-citizen-round-1/14905) aims to reward people and grassroots projects \
+            that have contributed to Gitcoin’s success, specifically by engaging with the wider ecosystem and community. \
+            An umbrella term could be ‘Gitcoin Community Engagement’. More specifically, we aim to reward work contributing directly to Gitcoin’s \
+            ‘most important things’ and our Purpose and Essential Intents. For example, contributions in the areas of educational content, data analysis, \
+            and support-oriented initiatives.")
+st.markdown("Gitcoin Citizens Round #1 is **[live](https://explorer.gitcoin.co/#/round/10/0x984e29dcb4286c2d9cbaa2c238afdd8a191eefbc)**! \
+            Donations open until June 27th 23:59 UTC")
+
 # while True:
 question = st.text_input("Question", placeholder="Enter your question here")
 
