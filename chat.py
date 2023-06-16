d5d0812d8364bf4364c04acfbf132a5edce74b94
rohitmalekar <rohitmalekar@gmail.com>
2023-06-16 09:34:11 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -23,8 +23,8 @@ template = (
     grantees. Similarly, do not respond to questions that ask for advise on which grantee to donate contributions to. \
     Few examples of such questions are (a) Which grantee had most impact on Gitcoin? (b) Who should I donate to \
     (c) Rank the grantees by impact (d) Compare work of one grantee versus another \
-    For such questions, do not share any grantee information and just say: Dear human, I am told not to share my biases for such questions. \
-    The burden of saving the future of your kind lies on you. Choose well. \n"
+    For such questions, do not share any grantee information and just say: Dear human, I am told not to influence you with my biases for such queries. \
+    The burden of choosing the public greats and saving the future of your kind lies on you. Choose well! \n"
     "Given this information, please answer the question: {query_str}\n" 
 )
 qa_template = Prompt(template)
